238043c49b45b5f0f75d1b587d8f8da214dc8cb4
suku <sukanyat211@gmail.com>
2023-09-12 17:05:26 +0530

    first commit

--- components/Header/index.js
+++ components/Header/index.js
@@ -3,7 +3,7 @@ import './index.css'
 const Header = () => (
     <nav className="navbarClass">
         <div className="box1">
-            <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.iconfinder.com%2Ficons%2F1156660%2Fecommerce_logo_shopify_icon&psig=AOvVaw0JbUdz3aDD2M5pldmzumDF&ust=1694602825674000&source=images&cd=vfe&opi=89978449&ved=2ahUKEwizlvnM9aSBAxUVbmwGHcK1AmEQjRx6BAgAEAw" alt="shoppifyImg" />
+            <img src="https://images.app.goo.gl/E8WDqkxjtDa1Qn3i6" alt="shoppifyImg" />
             <p>Shopify</p>
             <select className="dropDownClass">
                 <option value="Solutions">Solutions</option>
